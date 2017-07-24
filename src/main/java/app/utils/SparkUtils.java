79ecebf7397ce8732740a950c3965183a34c408d
Yair Galler <yairg@leadspace.com>
2017-07-24 21:54:09 +0300

    Import optimization

--- src/main/java/app/utils/SparkUtils.java
+++ src/main/java/app/utils/SparkUtils.java
@@ -1,11 +1,12 @@
 package app.utils;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.AbstractNCSARequestLog;
 import spark.embeddedserver.EmbeddedServers;
 import spark.embeddedserver.jetty.EmbeddedJettyFactory;
 
 public class SparkUtils {
-    public static void createServerWithRequestLog(org.apache.log4j.Logger logger) {
+    public static void createServerWithRequestLog(Logger logger) {
         EmbeddedJettyFactory factory = createEmbeddedJettyFactoryWithRequestLog(logger);
         EmbeddedServers.add(EmbeddedServers.Identifiers.JETTY, factory);
     }
