67b85463bd9d327e7932de9ae3c402ad52511ee7
Yair Galler <yair@next-insurance.com>
2018-06-09 08:48:19 +0300

    Upgrade to Spark 2.7.2

--- src/main/java/app/utils/EmbeddedJettyFactoryConstructor.java
+++ src/main/java/app/utils/EmbeddedJettyFactoryConstructor.java
@@ -1,8 +1,6 @@
 package app.utils;
 
 import org.eclipse.jetty.server.AbstractNCSARequestLog;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import spark.embeddedserver.jetty.EmbeddedJettyFactory;
 
 public class EmbeddedJettyFactoryConstructor {
@@ -13,19 +11,9 @@ public class EmbeddedJettyFactoryConstructor {
     }
 
     EmbeddedJettyFactory create() {
-        return new EmbeddedJettyFactory((maxThreads, minThreads, threadTimeoutMillis) -> {
-            Server server;
-            if (maxThreads > 0) {
-                int max = maxThreads > 0 ? maxThreads : 200;
-                int min = minThreads > 0 ? minThreads : 8;
-                int idleTimeout = threadTimeoutMillis > 0 ? threadTimeoutMillis : '\uea60';
-                server = new Server(new QueuedThreadPool(max, min, idleTimeout));
-            } else {
-                server = new Server();
-            }
+        EmbeddedJettyServerFactory embeddedJettyServerFactory = new EmbeddedJettyServerFactory(this);
 
-            server.setRequestLog(requestLog);
-            return server;
-        });
+        return new EmbeddedJettyFactory(embeddedJettyServerFactory);
     }
+
 }
